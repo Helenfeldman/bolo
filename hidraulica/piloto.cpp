62f76de2012cb845d90a68fccca29cb2a28cd27e
Helenfeldman <helenfm@outlook.com.br>
2020-06-17 09:53:55 -0300

    Create piloto.cpp

---
+++ hidraulica/piloto.cpp
@@ -0,0 +1,16 @@
+// Helen Feldman.
+#include<iostream>
+using namespace std;
+int main(){
+
+int A,B,C;
+cin >> A >> B >> C;
+if((B-A) < (C-B)){
+    cout << 1;
+}else if((B-A) > (C-B)){
+    cout << -1;
+}else{
+    cout << 0;
+}
+
+}
